a098d9a24c90a0604783a03227dda48bf5323c94
Andrew <mail2airisaac@gmail.com>
2025-11-20 23:12:28 +0400

    Updating venv

--- UI/tailwind.config.js
+++ UI/tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  darkMode: 'class',
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: '#030014'
+      }
+    }
   },
   plugins: [],
 }
-
